aead688f63f0e1f3d9fd6236681d4b23d047ab52
laik184 <hasan8665r@gmail.com>
2025-10-31 22:58:57 +0530

    Delete src/features/videoChat directory

--- src/features/videoChat/reportUser.js
+++
@@ -1,8 +0,0 @@
-export function reportUser(socket, isConnected, reportReason, setReportReason, setShowModal) {
-  if (reportReason.trim() && isConnected && socket) {
-    socket.emit('report-user', { reportReason });
-    setReportReason('');
-    setShowModal(false);
-    alert('Report submitted. Thank you for keeping our community safe.');
-  }
-}
