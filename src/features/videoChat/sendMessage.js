67f27c6ca29ccf5dc1709665d68dcabd580ceb65
laik184 <hasan8665r@gmail.com>
2025-10-31 22:54:21 +0530

    Delete src/features/videoChat directory

--- src/features/videoChat/sendMessage.js
+++
@@ -1,7 +0,0 @@
-export function sendMessage(socket, message, setMessages, setMessage) {
-  if (message.trim() && socket) {
-    setMessages(prev => [...prev, { text: message, sender: 'you' }]);
-    socket.emit('send-message', { message });
-    setMessage('');
-  }
-}
