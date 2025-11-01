e7820e9186b7252caeae850546857eee91d6c0ed
laik184 <hasan8665r@gmail.com>
2025-11-01 06:41:58 +0530

    Delete src/features/videoChat directory

--- src/features/videoChat/submitFeedback.js
+++
@@ -1,8 +0,0 @@
-export function submitFeedback(socket, feedback, setFeedback, setShowModal) {
-  if (feedback.trim() && socket) {
-    socket.emit('submit-feedback', { feedback });
-    setFeedback('');
-    setShowModal(false);
-    alert('Thank you for your feedback!');
-  }
-}
